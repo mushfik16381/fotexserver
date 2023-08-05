2255293f0709e4b2e2a48a757cd16eb81ce5dfae
mushfik16381 <ahmed.mushfik16381@gmail.com>
2023-08-05 10:27:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const server = app.listen(port, () => {
+    console.log(`Server is running: ${port}`);
+  });
+  
+  
+  module.exports = { app };
